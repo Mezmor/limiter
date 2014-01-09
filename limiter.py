89060c50cc9be7bfb813093cdf373d28a160c4fa
Mezmor <lepswiss@gmail.com>
2014-01-09 02:18:17 -0800

    Fixed allowance updates

--- limiter.py
+++ limiter.py
@@ -22,16 +22,18 @@ from datetime import datetime, timedelta
 def calls_per_sec(calls=1, seconds=1):
     def limiter(function):
         def wrapper(*args, **kwargs):
-            if not hasattr(wrapper, 'last_called'):
-                wrapper.last_called = datetime.now()
+            if not hasattr(wrapper, 'last_updated'):
+                wrapper.last_updated = datetime.now()
             # Are we calling it within X seconds of the last successful call?
             # If so, remove from our allowance
-            if (datetime.now() - wrapper.last_called) <= timedelta(seconds = seconds):
+            if (datetime.now() - wrapper.last_updated) <= timedelta(seconds = seconds):
                 wrapper.calls_allowed -= 1
                 # We don't have any calls left, abort!
-                if wrapper.calls_allowed < 0:                                          
+                if wrapper.calls_allowed < 0:
                     return None
-            wrapper.last_called = datetime.now()
+            else:
+                wrapper.calls_allowed = calls
+                wrapper.last_updated = datetime.now()
             return function(*args, **kwargs)
         wrapper.calls_allowed = calls
         return wrapper
